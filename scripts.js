3444139e95bf9f0fed52e36a5b6eb6703a3f8099
bigmacseth <bigmacseth@gmail.com>
2025-08-05 21:01:01 -0500

    i think i'm cookin here, added a memory array to display a string and execute once equals is pressed

--- scripts.js
+++ scripts.js
@@ -30,16 +30,39 @@ function operate(operator, num1, num2) {
 
 const viewCard = document.querySelector('.view-window');
 const numbers = document.querySelector('#numbers');
+const memoryCard = document.querySelector('#memory');
+
+const clearEntry = document.querySelector('#clearEntry');
+const clearAll = document.querySelector('#clearAll');
+const backspace = document.querySelector('#backspace')
 
 const listOfNumberButtons = document.querySelectorAll('.num-btn');
 const listOfFunctionButtons = document.querySelectorAll('.func-btn');
 const listOfOperatorButtons = document.querySelectorAll('.operator-btn');
 
+
+
+let memory = [];
+
 // iterate over the list of buttons with numbers and add an event listener to insert text into 'numbers'
 listOfNumberButtons.forEach(item => {
     let value = item.id;
-    item.addEventListener('click', (e) => {
+    item.addEventListener('click', () => {
         numbers.insertAdjacentText('beforeend', value);
-    })
-})
+    });
+});
 
+// iterate over the list of buttons with operands and add an event listener to:
+// 1. push the text content of 'numbers' into a memory array
+// 2. push the operand into the memory array
+// 3. clear the 'numbers' element
+// 4. display the memory array's contents in an element above the main 'numbers' element
+listOfOperatorButtons.forEach(item => {
+    let value = item.id;
+    item.addEventListener('click', () => {
+        memory.push(numbers.textContent)
+        memory.push(item.id);
+        numbers.textContent = '';
+        memoryCard.textContent = memory.join(' ');
+    });
+});
